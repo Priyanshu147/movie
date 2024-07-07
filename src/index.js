b6d08326999f27df133ef5b71691a75eeab9321a
Priyanshu147 <priyanshu035p@gmail.com>
2024-07-07 22:01:20 +0530

    Updated

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { render } from '@testing-library/react';
+
+ReactDOM(render(<App/>,document.getElementById('root')));
